c899e9eb58191e0ea6749ec73f503789b1d3c231
Somsatwik Deo <ssomsatwikdeo@gmail.com>
2025-12-07 13:03:21 +0530

    added mongob and port file

--- app.js
+++ app.js
@@ -1,54 +1,58 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 const path = require("path");
+const http = require("http");
+const socketio = require("socket.io");
 
-const mongoose = require("mongoose");
 
 mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log("MongoDB connected"))
-  .catch(err => console.log(err));
+  .catch(err => console.log("MongoDB Error:", err));
 
-  const locationSchema = new mongoose.Schema({
-    userId: String,
-    latitude: Number,
-    longitude: Number,
-    timestamp: { type: Date, default: Date.now }
+const locationSchema = new mongoose.Schema({
+  userId: String,
+  latitude: Number,
+  longitude: Number,
+  timestamp: { type: Date, default: Date.now }
 });
 
 const Location = mongoose.model("Location", locationSchema);
 
 
-
-const http = require("http");
-const socketio = require("socket.io");
-
 const server = http.createServer(app);
 const io = socketio(server);
 
 app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")));   // FIXED
+app.use(express.static(path.join(__dirname, "public")));
+
 
 io.on("connection", function(socket) {
-    socket.on("send-location", function(data) {
 
-        Location.create({
-    userId: socket.id,
-    latitude: data.latitude,
-    longitude: data.longitude
-});
-
-        io.emit("receive-location", {id:socket.id, ...data});
+  socket.on("send-location", function(data) {
+    Location.create({
+      userId: socket.id,
+      latitude: data.latitude,
+      longitude: data.longitude
     });
-   socket.on("disconnect", function() {
-        io.emit("user-disconnected", socket.id);
+
+    io.emit("receive-location", { id: socket.id, ...data });
+  });
+
+  socket.on("disconnect", function() {
+    io.emit("user-disconnected", socket.id);
+  });
 });
+
+
+app.get("/", function(req, res) {
+  res.render("index");
 });
-app.get("/", function (req, res) {
-    res.render("index");
-});
+
 
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-    console.log("Server running on port " + PORT);
+  console.log("Server running on port " + PORT);
 });
